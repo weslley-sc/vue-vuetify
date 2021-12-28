d7a16d82e4a8449bd4735777d96092d6697a5a83
weslley-sc <weslleydasilvacostaa@gmail.com>
2021-12-28 11:00:13 -0300

    app-bar + login finalizados

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,91 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="primary">
+      <v-app-bar-title>Vue + Vuetify</v-app-bar-title>
       <v-spacer></v-spacer>
-
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        v-for="link in links"
+        :key="link"
+
+        color="white"
         text
+        rounded
+        class="my-2"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        {{ link }}
       </v-btn>
     </v-app-bar>
-
+    <!-- Login -->
     <v-main>
-      <HelloWorld/>
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title><h1 class="display-1">Login</h1></v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Username"
+              prepend-icon="mdi-account-circle"
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="exibir ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="exibir ? 'text' : 'password'"
+              @click:append="showhide"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-main>
+      <v-footer
+    color="primary lighten-1"
+    padless
+
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    exibir: false,
+    links: ["Home", "Login", "Cadastro"],
   }),
+  methods: {
+    showhide() {
+      this.exibir = !this.exibir;
+    },
+  },
 };
 </script>
